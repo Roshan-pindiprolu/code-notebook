2e3fdb9178dfe33bdaf3d5f29fe131c4ebb6a43f
Roshan Pindiprolu <roshan@yarken.com>
2025-06-10 13:02:37 +0530

    Started the Python, Init Commit

---
+++ Python/Basic Python/Comments/index.py
@@ -0,0 +1,12 @@
+# This is the single line comment
+print("Hello Everyone")
+
+# For Mutli-line
+# we either use this "#" 
+print("Let's Learn Python together")
+
+'''
+    We can also use 
+    docstring as comments too
+'''
+print("Let's go")
